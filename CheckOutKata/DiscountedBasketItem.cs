6198b3a8c477a73d869a69071b9d6f62a299db71
reidca <carlandrewreid@hotmail.com>
2014-10-30 15:33:31 +0000

    Added untracked items

---
+++ CheckOutKata/DiscountedBasketItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckOutKata
+{
+    public class DiscountedBasketItem : IBasketItem
+    {
+
+        Product _product;
+        DateTime _dateCreated;
+        bool _hasBeenDiscounted;
+        string _discountApplied;
+
+        public DiscountedBasketItem(IBasketItem undiscountedBasketItem, DiscountRule discountApplied)
+        {
+            this._product = undiscountedBasketItem.Product;
+            this._dateCreated = undiscountedBasketItem.DateCreated;
+            this._hasBeenDiscounted = true;
+            this._discountApplied = discountApplied.Name;
+        }
+
+        public DateTime DateCreated
+        {
+            get { return _dateCreated; }
+        }
+
+        public Product Product
+        {
+            get { return _product; }
+        }
+
+        public bool HasBeenDiscounted
+        {
+            get { return _hasBeenDiscounted; }
+        }
+
+        public string DiscountApplied
+        {
+            get { return _discountApplied; }
+        }
+    }
+}
---
+++ CheckOutKata/PricingEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckOutKata
+{
+    public static class PricingEngine
+    {
+        internal static Dictionary<Product, Money> PriceList;
+
+        static PricingEngine()
+        {
+            PriceList = new Dictionary<Product, Money>() 
+            {
+                {new Product("A"), new Money(5)},
+                {new Product("B"), new Money(10)},
+                {new Product("C"), new Money(20)},
+                {new Product("D"), new Money(50)}
+            };
+        }
+
+    }
+}
